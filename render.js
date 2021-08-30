0fd176203c59fae5c2e7de62e922968e32839e8d
Shubham Khandelwal <beeshub7@gmail.com>
2021-08-31 03:39:04 +0530

    fix loading of css, js scripts before making an assertion in tests

--- render.js
+++ render.js
@@ -8,9 +8,15 @@ const {JSDOM} = jsdom;
 const render = async filename => {
     const filePath = path.join(process.cwd(), filename);
 
-    return await JSDOM.fromFile(filePath, {
-        runScripts: 'dangerously',
-        resources: 'usable'
+    const dom = await JSDOM.fromFile(filePath, {
+        runScripts: "dangerously",
+        resources: "usable"
+    });
+
+    return new Promise((resolve, reject) => {
+        dom.window.document.addEventListener("DOMContentLoaded", () => {
+            resolve(dom);
+        });
     });
 };
 
